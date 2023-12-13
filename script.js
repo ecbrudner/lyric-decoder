aac27e93c405bd8ca178e33f020e82d3112755f7
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2023-12-12 21:37:30 -0600

    formatted lyrics to be added to webpage and added API url for album art

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ async function getLyrics(){
         var trackId= resultObj.hits[0].result.id;
         console.log(trackId);
 
+        //use trackid to get lyrics
+
         const url2 = 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id='+ trackId+'&per_page=10&page=1';
         const options2 = {
 	        method: 'GET',
@@ -48,6 +50,35 @@ async function getLyrics(){
             console.log(resultObj2);
             var lyrics= resultObj2.lyrics.lyrics.body.html;
             console.log(lyrics);
+
+            //trim and display lyrics
+            lyricOutputEl.innerHTML= lyrics;
+            var lyricsText= lyricOutputEl.textContent.trim();
+            console.log(lyricsText);
+
+            lyricOutputEl.innerHTML= lyricsText;
+
+
+
+        } catch (error) {
+	        console.error(error);
+        }
+
+        //use track id to get album art
+        const url3 = 'https://genius-song-lyrics1.p.rapidapi.com/album/appearances/?id='+ trackId +'&per_page=20&page=1';
+        const options3 = {
+	        method: 'GET',
+	        headers: {
+		        'X-RapidAPI-Key': '6e80b305f6mshb4c48ce28d66fabp1ee768jsn39199b79955f',
+		        'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+	        }
+        };
+
+        try {
+	        const response = await fetch(url3, options3);
+	        const result = await response.text();
+	        console.log(result);
+
         } catch (error) {
 	        console.error(error);
         }
